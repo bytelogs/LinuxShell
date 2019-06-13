5ed52aaec0d16970a17c4284e72202adf1fceed7
balich.chen <balich.chen@outlook.com>
2019-06-13 13:58:05 +0800

    增加scp-id-pub.sh
    增加scp-id-pub.sh，于批量拷贝当前用户公钥到远程主机，实现免密通信

---
+++ scp-id-pub.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# 
+# FileName: scp-id-pub.sh
+# DATE: 2019-06-13
+# Author: Balich
+# DESC: 将当前用户下的公钥(id_rsa.pub)拷贝到远程主机
+# yum install expect -y
+
+## 远程主机列表,一行一台主机IP或者主机名
+HOSTLIST="
+10.80.210.110
+"
+## 远程主机用户
+USER="root"
+## 远程主机用户密码
+PASSWD="38039260@XINsec.Com.cn"
+
+## 自动交互执行程序
+for HOST in $HOSTLIST;do
+/usr/bin/expect << EOF
+spawn ssh-copy-id $USER@$HOST
+expect {
+"yes/no" {send "yes\r"}
+}
+expect {
+"password:" {send "$PASSWD\r"}
+}
+expect eof
+EOF
+done
+
