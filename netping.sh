896185a55a82f084f5c27a2c88d0dcd9b51d28d8
balich.chen <balich.chen@outlook.com>
2019-06-13 15:11:29 +0800

    Update netping.sh
    使用多线程

--- netping.sh
+++ netping.sh
@@ -2,20 +2,18 @@
 # FileName:netping.sh 
 # DATE: 2019-04-24
 # Author: Balich
-# DESC: ping 指定网段内IP地址，打印出IP的状态
+# DESC: ping 指定网段内IP地址，打印出在线的IP状态并写入到文件
+#       使用多线程
 
 
 # 要检查的IP网段
 SUBIP="10.80.210"
 
-while true;do
-    for ip in {0..255};do
-        ping -c 2 -w 2 ${SUBIP}.${ip} &> /dev/null
-        if [ $? -eq 0 ]; then
-            echo -e "\033[32;40m ${SUBIP}.${ip} is UP. \033[0m"
-        else
-            echo -e "\033[31m ${SUBIP}.${ip} is DOWN. \033[0m"
-        fi
-    done
-        break
+for ip in {0..255};do
+    {
+    ping -c 2 -w 2 ${SUBIP}.${ip} > /dev/null && echo ${SUBIP}.${ip}|tee -ai upip.log
+    } &
 done
+wait
+
+
